1ed44a2943ed6c9d10efd84ac9159e0542dda598
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-17 20:36:16 +0530

    add snake head in js, css file

--- Game.js
+++ Game.js
@@ -1,17 +1,20 @@
 let board = document.querySelector(".board");
 let FoodX;
 let FoodY;
+let SnakeX;
+let SnakeY;
 
-
-function randomFoodPosition(){
-    FoodX=Math.floor(Math.random()*24)+1;
-    FoodY=Math.floor(Math.random()*24)+1;
-   
+function randomFoodPosition() {
+  FoodX = Math.floor(Math.random() * 24) + 1;
+  FoodY = Math.floor(Math.random() * 24) + 1;
+  SnakeX = Math.floor(Math.random() * 24) + 1;
+  SnakeY = Math.floor(Math.random() * 24) + 1;
 }
 
-function main(){
-    randomFoodPosition()
-  let setHtml = `<div class="food" style="grid-area: ${FoodY}/${FoodX}; "></div>`
-   board.innerHTML=setHtml;
+function main() {
+  randomFoodPosition();
+  let setHtml = `<div class="food" style="grid-area: ${FoodY}/${FoodX}; "></div>`;
+  setHtml += `<div class="snake-head" style="grid-area: ${SnakeY}/${SnakeX};"></div>`;
+  board.innerHTML = setHtml;
 }
 main();
