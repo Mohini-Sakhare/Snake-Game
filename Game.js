9cb6c10eaed917776c8f12ac9b67be415b264060
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-18 17:59:55 +0530

    add snakeBody logic

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 let board = document.querySelector(".board");
 let FoodX;
 let FoodY;
+let snakeBody=[]
 let SnakeX=3;
 let SnakeY=5;
 let velocityX=0;
@@ -31,15 +32,24 @@ function moveSnake(e){
 function main() {
  if(SnakeX===FoodX && SnakeY===FoodY){
     randomFoodPosition();
+    snakeBody.push([FoodX,FoodY])
+    console.log(snakeBody);
  }
 
+ for(let i=snakeBody.length-1; i>0; i--){
+  snakeBody[i]=snakeBody[i-1]
+ }
+let setHtml = `<div class="food" style="grid-area: ${FoodY}/${FoodX}; "></div>`;
   SnakeX+=velocityX;
   SnakeY+=velocityY;
-  let setHtml = `<div class="food" style="grid-area: ${FoodY}/${FoodX}; "></div>`;
-  setHtml += `<div class="snake-head" style="grid-area: ${SnakeY}/${SnakeX};"></div>`;
+  snakeBody[0]=[SnakeX,SnakeY]
+  for(let i=0; i<snakeBody.length; i++){
+     setHtml += `<div class="snake-head" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]};"></div>`;
+  }
   board.innerHTML = setHtml;
 }
 randomFoodPosition();
+setInterval(main, 300)
 main();
 
 document.addEventListener("keydown", moveSnake)
